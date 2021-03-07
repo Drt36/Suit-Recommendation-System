e609d16440a883816928f80cebb711853c1f0222
Dharma Raj Thanait <53378643+Drt3636@users.noreply.github.com>
2021-03-07 22:06:02 +0545

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ def recommend():
     result=tmswdr.recommend_cosine(request.args.get('Design_codein'))
     return jsonify(result)
 
+@app.route('/sample', methods=['GET'])
+def getsample():
+    result=tmswdr.sample_row()
+    return jsonify(result)
+
 @app.route('/')
 def test():
-    return "Hurray!, Working..."
+    return "Hurray!, Recommendation System is Working..."
 if __name__=='__main__':
     app.run(port = 5000, debug =True)
 
